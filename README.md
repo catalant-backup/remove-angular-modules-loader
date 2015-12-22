52c99e3622fbec959f478c27dbd65ea8efce6610
Misha Koryak <mkoryak@gmail.com>
2015-12-22 16:14:44 -0500

    add whiteList and blackList for modules. mostly this makes e2e/unit testing work

--- README.md
+++ README.md
@@ -13,12 +13,27 @@ in your webpack config's loaders section:
 ```js
 {
     test: /\/angular(.min)?\.js$/,
-    loader: 'remove-angular-modules?moduleName=modules-are-bad'
+    loader: 'remove-angular-modules'
 },
 ```
 
-moduleName is optional, default module name is 'app'.
+moduleName is optional, default module name is 'app'. It is recommended that you update your html with this module name.
 
+# Query params
+
+ - `moduleName` name of the module that everything will go into
+ - `whiteList[]` allow these modules to be created under their original module name
+ - `blackList[]` ignore creation of these modules. no code registered to these modules will ever run
+ - `testing` valid values: 'unit' or 'e2e'. ngMock contains both types of modules, so this will properly configure the blacklist property for you. Use this only when testing with ngMock or ngMockE2E
+
+example
+
+```js
+{
+    test: /\/angular(.min)?\.js$/,
+    loader: 'remove-angular-modules?moduleName=hourlynerd&whiteList[]=foo&whiteList[]=bar&blackList[]=skillbridge&testing=e2e'
+}
+```
 
 # How it works
 
@@ -26,7 +41,7 @@ It patches angular to put everything into a single module*.
 You no longer need to define module dependencies when creating modules, or names for that matter.
 
 
-* - `protractorBaseModule_` remains its own module because otherwise protractor tests don't work.
+* - `protractorBaseModule_` remains its own module because otherwise protractor tests don't work. (you can still blacklist it)
 
 
 This is now valid:
@@ -41,7 +56,7 @@ Now you can focus on requiring angular things with webpack just like you require
 
 # Secret Sauce
 
-The magic happens when we append the following script to the end of angular.js
+The magic happens when we append the following script to the end of angular.js (ok, its a bit longer now...)
 
 ```js
 (function(a, moduleName) {
--- index.js
+++ index.js
@@ -1,24 +1,51 @@
 var utils = require('loader-utils');
 
-var tpl = function(moduleName) {
-    return "\n\
-(function(a, moduleName) {\n\
-    // remove angular module system, everything is now in the `moduleName` module ;)\n\
-    // except for protractor which does something funny and cant party with us\n\
-    var _module = a.bind(a, a.module);\n\
-    var m = _module(moduleName, []);\n\
-    a.module = function (n, d) { return (n === 'protractorBaseModule_') ? _module(n, d) : m;};\n\
-    document.querySelectorAll('[ng-app]')[0].setAttribute('ng-app', moduleName);\n\
-})(window.angular, '"+moduleName+"');\n\
-";
-};
+var tplFn = function(moduleName) {/*
+ (function(a, moduleName, whiteList, blackList) {
+     // remove angular module system, everything is now in the `moduleName` module ;)
+     var mockMethods = ['animation', 'config', 'constant', 'controller', 'decorator', 'run',
+                        'directive', 'factory', 'filter', 'provider', 'service', 'value'];
+     var dummy = function(){ return dummy };
+     for(var i in mockMethods){
+        dummy[mockMethods[i]] = dummy;
+     }
+     var _module = a.bind(a, a.module);
+     var m = _module(moduleName, []);
+     a.module = function (n, d) {
+         if(~blackList.indexOf(n)){
+            return dummy;
+         }
+         if(~whiteList.indexOf(n)){
+            return _module(n, d);
+         }
+         return m;
+     };
+     document.querySelectorAll('[ng-app]')[0].setAttribute('ng-app', moduleName);
+ })(window.angular, §1, §2, §3);
+ ;
+ */};
+var tpl = function(opt){
+    var str = /\/\*!?(?:\@preserve)?[ \t]*(?:\r\n|\n)([\s\S]*?)(?:\r\n|\n)[ \t]*\*\//.exec(tplFn.toString())[1];
+    var whiteList = opt.whiteList || [];
+    var blackList = opt.blackList || [];
+    whiteList.push('protractorBaseModule_');
+    if(opt.testing == 'e2e'){
+        blackList.push('ngMock');
+    }
+    if(opt.testing == 'unit'){
+        blackList.push('ngMockE2E');
+    }
+    str = str.replace('§1', JSON.stringify(opt.moduleName || 'app'));
+    str = str.replace('§2', JSON.stringify(whiteList));
+    str = str.replace('§3', JSON.stringify(blackList));
+    return str;
+}
 
 module.exports = function (content) {
     this.cacheable && this.cacheable();
     //lets make double sure we only add it on the right stuff
     if(this.resourcePath.match(/\/angular(\.min)?\.js$/)){
-        var opt = utils.parseQuery(this.query);
-        return content + tpl(opt.moduleName || 'app');
+        return content + tpl(utils.parseQuery(this.query));
     }
     return content;
 };
